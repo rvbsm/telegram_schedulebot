e783b397c8905eba2da88c910c8000581ed00864
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2021-11-07 18:10:44 +0200

    Value

--- main.py
+++ main.py
@@ -174,4 +174,4 @@ async def on_startup(dp):
 
 if __name__ == "__main__":
     pg.createTables()
-    executor.start_webhook(dispatcher=dp, webhook_path=cnf.WebhookConfig.WEBHOOK_PATH, skip_updates=False, on_startup=on_startup, host='0.0.0.0')
+    executor.start_webhook(dispatcher=dp, webhook_path=cnf.WebhookConfig.WEBHOOK_PATH, skip_updates=False, on_startup=on_startup, host='0.0.0.0', port=os.getenv("PORT"))
